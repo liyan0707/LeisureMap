b0dbde4815edc9c6d689fb65f1a54bd84ec600df
liyan0707 <728762713@qq.com>
2018-07-28 16:46:33 +0800

    add

--- LeisureMap/MapFlow/MapViewController.swift
+++ LeisureMap/MapFlow/MapViewController.swift
@@ -13,18 +13,39 @@ import CoreLocation
 class MapViewController: UIViewController ,CLLocationManagerDelegate , MKMapViewDelegate {
 
     @IBOutlet weak var mapview: MKMapView!
-    let regionRadius : CLLocationDistance = 100
+    
+    let locationManager = CLLocationManager()
+    
+    let regionRadius : CLLocationDistance = 1000
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        if CLLocationManager.locationServicesEnabled(){
+            
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            
+        }
+        
+        
+        
+        mapview.delegate = self
         mapview.showsUserLocation = true
+        
+        //centerMapOnLocation
 
-        // Do any additional setup after loading the view.
     }
     
     func  centerMapOnLocation(location : CLLocation){
-        let  coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius , regionRadius )
+        
+        let  coordinateRegion = MKCoordinateRegionMakeWithDistance( location.coordinate, regionRadius , regionRadius )
      
         mapview.setRegion(coordinateRegion, animated: true)
     }
@@ -35,6 +56,17 @@ class MapViewController: UIViewController ,CLLocationManagerDelegate , MKMapView
         // Dispose of any resources that can be recreated.
     }
     
+    func locationManger(_ manager: CLLocationManager ,didUpdateLocations  locations:[CLLocation]){
+        
+        guard let locValue : CLLocationCoordinate2D = manager.location?.coordinate   else {
+            return
+            
+        }
+        
+        print("latitude:\(locValue)")
+        
+        centerMapOnLocation(location:manager.location! )
+    }
 
     /*
     // MARK: - Navigation
